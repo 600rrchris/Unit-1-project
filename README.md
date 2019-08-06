76ed3b4efa9d4716881b4d4a3225401c56f3bfcb
MacBook Pro <macbookpro@Chriss-Macbook.local>
2019-08-06 13:48:46 -0500

    randomize numbers

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 
 
-Player should be able to to start game with next round button. Once pressed 6 random boxes will 
+Player should be able to to start game with next round button. Once pressed 6 random boxes will show numbers 1-6 and disappear after set time. Player will try and pick numbers in order starting from 1 ending with 6. If player successfully guesses in order they will be able to click next round and try again with a shorter timer.
--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
 // state
-
+var order = [1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+    console.log(array);
+ }
 //cached elements
+var board, guess, win
 
 //event listeners
-document.getElementById('board').addEventListener('click', playerGuess);
-document.querySelector('button').addEventListener('click',  nextRound);
+document.getElementById('round').addEventListener('click', nextRound);
 document.getElementById('reset').addEventListener('click', resetGame);
-
+var boxes = document.querySelectorAll('.squares');
+boxes.forEach(el =>{
+    el.addEventListener('click', playerGuess);
+  })
 
 //functions
+init ();
+
+function init(){
+   
+};
 function render() {
 
 };
 
 function nextRound() {
-new Array(36).fill(null);
-
+    shuffle(order);
+    // let startingArray =  Array(36).fill().map(() =>  Math.floor(Math.random() * order.length + 1))
+    // console.log(startingArray);
+    boxes.forEach((el, idx) => {
+    el.innerText = `${order[idx]}`
+    });
 };
 
-function playerGuess() {
-console.log(playerGuess);
+function playerGuess(evt) {
+console.log(evt.target);
 };
 
 function resetGame() {
 console.log(resetGame);
-};
-//
+}
