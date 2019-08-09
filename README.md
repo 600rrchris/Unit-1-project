69c363f4b045d65bae3d86332b17c65b686fd19c
MacBook Pro <macbookpro@Chriss-Macbook.local>
2019-08-09 11:16:27 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # `Unit 1 Project` folder
 
 
-
+#objective
 Player should be able to to start game with next round button. Once pressed 6 random boxes will show numbers 1-6 and disappear after set time. Player will try and pick numbers in order starting from 1 ending with 6. If player successfully guesses in order they will be able to click next round and try again with a shorter timer.
 
-###create generic board
-###add round and reset button
-###add js guide
-###first objective create base array to have random number pushed into
-###push array into grid cells
-###fade out numbers with timer
-###have number appear in boxes with the push of "round" button
-####have player able to click on random boxes to guess number
-####save players guess in new array
-####compare guess array to winner array
-###if match alert winner
+#####create generic board
+#####add round and reset button
+#####add js guide
+#####first objective create base array to have random number pushed into
+#####push array into grid cells
+#####fade out numbers with timer
+#####have number appear in boxes with the push of "round" button
+#####have player able to click on random boxes to guess number
+#####save players guess in new array
+#####compare guess array to winner array
+#####if match alert winner
 ####if does not match alert try again
-###click on reset button should empty out box, guess array and base array
+#####click on reset button should empty out box, guess array and base array
 
-80s background theme
-music
-timer down
+
--- js/main.js
+++ js/main.js
@@ -1,96 +1,116 @@
 // state
-var order = [1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
+var order = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
 const winRound = ["1", "2", "3", "4", "5", "6"];
 var guessRound =[];
 var finalArr = [];
-
-//cached elements
-var board, guess, win
+const laser = new Audio("sounds/laser-sound.mp3");
+const loser = new Audio("sounds/loser-sound.mp3");
+const timeSound = new Audio("sounds/countdown.mp3");
+const gameStart = new Audio("sounds/game-start.mp3");
+var boxes = document.querySelectorAll('.squares');
+var cat = 'https://i.imgur.com/GFvYUSm.png'
 
 //event listeners
 document.getElementById('round').addEventListener('click', nextRound);
 document.getElementById('reset').addEventListener('click', resetGame);
-document.getElementById('check').addEventListener('click', match);
-var boxes = document.querySelectorAll('.squares');
-
+var title = document.getElementsByTagName('h1');
 //functions
-init ();
-
-function init(){
-    
-};
-function render() {
-    
-};
 
 function nextRound() {
+    timer(10);
+    timeSound.play();
     var boxes = document.querySelectorAll('.squares');
-boxes.forEach(el =>{
+    boxes.forEach(el =>{
     el.addEventListener('click', playerGuess);
 });
     shuffle(order);
     boxes.forEach((el, idx) => {
         el.innerText = `${order[idx]}`
+
     });
     boxes.forEach((el, idx) => {
-        el.style.transition = '5s';
+        el.style.transition = '10s';
         el.style.opacity = 0;
     });
 };
 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
-    console.log(array);
  }
 
 function playerGuess(evt) {
     guessRound.push(evt.target.innerText);
     evt.target.style.transition = '1s';
     evt.target.style.opacity = 1;
-    evt.target.style = "color: purple; border: 5px solid blue; border-radius: 25%"
-    //  if (guessRound.length >= 6) {
-        if (guessRound.length === winRound.length) {
-            for(i = 0; i < winRound.length; i++){
-                if(guessRound[i] === winRound[i]) {
-         resetGame();
-                }
-            };
-        return;
-    }
-    console.log(guessRound);
- };
-
-
-    function match() { 
-        if (guessRound.length === winRound.length) {
-            for(i = 0; i < winRound.length; i++){
-                if(guessRound[i] === winRound[i]) {
-                    finalArr.push(true);
-              } else {
-                  finalArr.push(false);
-              }
-            }
+    evt.target.style = "color: white; border: 5px solid #F0ADF5; border-radius: 25%"
+        if (evt.target.innerText == '1') {
+            laser.play();
         };
-        winner();
+        if (evt.target.innerText == "0") {
+            loser.play();
+        };
+        if (guessRound.length === winRound.length) {
+            for(i = 0; i < winRound.length; i++){
+                winner();
+                timeSound.pause();
+            };    
     }
     
+ };
+
+    
     function winner() {
-        if (finalArr.every(el => el === true)) {
-            alert ('about fucking time')
-    } else 
-    // if (finalArr.every(el => el === false))
-    {
+        if (finalArr.every(el => el === 1)) {
+            alert ('meow')
+    } else  {
         alert ('sucks')
+        }
     }
+
+function stop ( ) {
+        for(i = 0; i < winRound.length; i++){
+            if(guessRound[i] == "0"){
+                gameStart.play();
+            }
+        }
     }
+stop();
+
 function resetGame() {
-   order = [1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
+   order = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
    guessRound = [];
    boxes.forEach((el, idx) => {
        el.innerHTML = [];
        el.style.opacity = 1;
-       el.style = "color: ; border:1px solid black"
-       el.removeEventListener('click', playerGuess);    
+       el.style = "color: ; border:1px solid white"
+       el.removeEventListener('click', playerGuess);
+       timeSound.pause();
+       timer(0);    
    })
 };
+let finalCountdown;
+const timerDisplay = document.getElementById('timer');
+
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+
+    finalCountdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now())/1000);
+        if(secondsLeft <= 0) {
+            clearInterval(finalCountdown);
+            return;
+        }
+    displayTimeLeft(secondsLeft);
+}, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds <10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
 
